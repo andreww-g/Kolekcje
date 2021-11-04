0246e61e69d56a71786fe0d390e5cc83b4032a7a
locapeesh <92260919+locapeesh@users.noreply.github.com>
2021-11-04 12:58:22 +0100

    First commit

---
+++ 262701/src/lab2/Movie.java
@@ -0,0 +1,38 @@
+package lab2;
+
+public class Movie {
+    private String MovieName;
+    private String ProducerName;
+    private int ReleaseDate;
+    private String GlobalRatings;
+    
+
+
+    Movie(String MovieName, String ProducerName, int ReleaseDate, String GlobalRatings)
+    {
+        this.MovieName = MovieName;
+        this.ProducerName = ProducerName;
+        this.ReleaseDate = ReleaseDate;
+        this.GlobalRatings = GlobalRatings;
+    }
+
+    public String getMovieName()
+    {
+        return MovieName;
+    }
+
+    public String getLastName()
+    {
+        return ProducerName;
+    }
+
+    public int getReleaseDate()
+    {
+        return ReleaseDate;
+    }
+
+    public String getGlobalRatings()
+    {
+        return GlobalRatings;
+    }
+}
